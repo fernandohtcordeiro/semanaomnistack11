e89b7b1d9bcb0304b26a3ce912080160042ba9cf
fernandohtcordeiro <fernando.htcordeiro@gmail.com>
2020-04-01 16:20:27 -0300

    Validação de rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ DELETE deletando informação do back-end
   
 const express = require('express');
 const cors = require('cors')
-
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -19,4 +19,6 @@ const app = express();
 app.use(cors()); //apermit que todas as app frontend acessem o backend
 app.use(express.json()); //Converte o json para javascript
 app.use(routes);
+app.use(errors());
+
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 // agrupamento de arquivos "Controller.js"
 
 const express = require('express');
+const {celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -12,12 +13,34 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);   // Listar
-routes.post('/ongs', OngController.create); // Cadastrar
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.create); // Cadastrar
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);         // Listar
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.index);         // Listar
+
 routes.post('/incidents', IncidentController.create);       // Cadastrar
-routes.delete('/incidents/:id', IncidentController.delete); // Apagar
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}),IncidentController.delete); // Apagar
 
 module.exports = routes;
